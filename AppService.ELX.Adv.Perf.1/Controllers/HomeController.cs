64bb1ca6ab2418de9a0948d8d999637afc18e499
juzhu <juzhu@SHA-JUZHU-BOOK>
2021-08-25 09:44:03 +0800

    Add project files.

---
+++ AppService.ELX.Adv.Perf.1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using AppService.ELX.Adv.Perf._1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppService.ELX.Adv.Perf._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _logger.LogInformation("In constructor of HomeController");
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("In Index() of HomeController");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            _logger.LogInformation("In Privacy() of HomeController");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            _logger.LogInformation("In Error() of HomeController");
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AppService.ELX.Adv.Perf.1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppService.ELX.Adv.Perf._1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
